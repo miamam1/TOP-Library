108160b09f892f8f2173a50af96ca24fa9a058b9
miamam1 <theshpeecrab@gmail.com>
2023-11-23 15:32:12 +0000

    Added: -Most app funciionality, can now: -Add books -Remove books -Toggle if a book has been read or not Known issues/To Do: - UI issues, cards for books dont look very appealing, and break when resizing the window even a little bit.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const booksContainer = document.getElementById("booksContainer");
+const dialog = document.querySelector("dialog")
+const addBook = document.getElementById("addBook");
+const closeForm = document.getElementById("closeForm");
+const confirmBtn = document.getElementById("confirmBtn");
+const formTitle = document.getElementById("formTitle");
+const formAuthor = document.getElementById("formAuthor");
+const formPages = document.getElementById("formPages")
+const readCheck = document.getElementById("readCheck")
 
 
 
@@ -17,9 +26,127 @@ function book(title, author, pages, read) {
 function addToLibrary(title, author, pages, read) {
      let newBook = new book(title, author, pages, read)
      return myLibrary.push(newBook)
-
 }
 
+function displayBooks() {
+    booksContainer.innerHTML = "";
+    for(let i = 0;i < myLibrary.length; i++) {
+        const book = document.createElement("div")
+        book.classList.add("card")
+ 
+
+        const title = document.createElement("p")
+        title.classList.add("title")
+        title.textContent = myLibrary[i].title
+        
+        const author = document.createElement("p")
+        author.classList.add("author")
+        author.textContent = myLibrary[i].author
+
+        const pages = document.createElement("p")
+        pages.classList.add("pages")
+        pages.textContent = myLibrary[i].pages
+       
+        const read = document.createElement("p")
+        read.classList.add("read")
+        read.textContent = myLibrary[i].read
+
+        const remove = document.createElement("button")
+        remove.classList.add("remove")
+        
+        remove.addEventListener("click", () => {
+            myLibrary.splice(i, 1);
+            book.remove();
+            console.log(myLibrary)
+        })
+
+        const cardRead = document.createElement("button")
+        cardRead.textContent = "Complete?"
+        cardRead.classList.add("cardRead")
+        cardRead.onclick = function() {
+            if(read.textContent == "Read") {
+                read.textContent = "Not read"
+            } else {
+                read.textContent = "Read"
+            }
+        }
+       
+
+        
+       
+        book.appendChild(title)
+        book.appendChild(author)
+        book.appendChild(pages)
+        book.appendChild(read)
+        book.appendChild(cardRead)
+        book.appendChild(remove)
+        booksContainer.appendChild(book)    
+    }
+}
+
+
+
+
+
+
+addBook.addEventListener("click", () => {
+    formTitle.value = ""
+    formAuthor.value = ""
+    formPages.value = ""
+    readCheck.value = ""
+    readCheck.checked = false;
+    formTitle.style.border = "1px solid black"
+        formAuthor.style.border = "1px solid black"
+        formPages.style.border = "1px solid black"
+    dialog.showModal();
+   
+})
+
+
+closeForm.addEventListener("click", () => {
+    dialog.close();
+})
+
+
+confirmBtn.addEventListener('click', () => {
+    if(readCheck.checked) {
+        readCheck.textContent = "Read"
+    } else {
+         readCheck.textContent = "Not read"
+    }
+    if(formTitle.value === "" ||   formAuthor.value === "" || formPages.value === "" ) {
+        formTitle.style.border = "1px solid red"
+        formAuthor.style.border = "1px solid red"
+        formPages.style.border = "1px solid red"
+        
+
+    } else {
+        dialog.close(addToLibrary(formTitle.value, formAuthor.value, formPages.value, readCheck.textContent))
+        displayBooks()
+
+    }
+
+
+
+
+})
+
+
+
+
+
+
+
+addToLibrary("Macbeth", "Shakespeare", "298", "Not read")
+addToLibrary("Romeo and Juliet", "Shakespeare", "240", "Read")
+addToLibrary("Stoicism", "Some stoic dude", "199", "Read")
+
+
+
+
+
+
+displayBooks()
 
 
 
