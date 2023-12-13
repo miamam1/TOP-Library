0d8f14b78506a7090f652905240905c9df529247
miamam1 <theshpeecrab@gmail.com>
2023-12-13 13:18:36 +0000

    Changed book constructor to class

--- script.js
+++ script.js
@@ -12,15 +12,17 @@ const readCheck = document.getElementById("readCheck")
 
 const myLibrary = [];
 
-function book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        return title + author + pages + read;
-    }
+class book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.info = function () {
+            return title + author + pages + read;
+        };
 
+    }
 }
 
 function addToLibrary(title, author, pages, read) {
